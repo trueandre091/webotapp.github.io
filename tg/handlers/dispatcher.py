3c08dd1d71561f0ec4671cb52e3292033ebfbb4a
andre <151407275+trueandre091@users.noreply.github.com>
2024-11-21 23:05:29 +0200

    try

--- tg/handlers/dispatcher.py
+++ tg/handlers/dispatcher.py
@@ -6,7 +6,7 @@ from tg.bot import dp, bot
 async def callback_start(message: Message, state: FSMContext):
     print(await bot.get_updates())
     user = message.from_user
-    web_app_info = WebAppInfo(url="https://trueandre091.github.io/firsty.github.io/")
+    web_app_info = WebAppInfo(url="https://trueandre091.github.io/webotapp.github.io/about/")
     markup = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Open Web App", web_app=web_app_info)]])
     await message.answer(f'Hello, {user.first_name}!', reply_markup=markup)
 
